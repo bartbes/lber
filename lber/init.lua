292110f907cdd5b7665177326254bcaa27219a8a
Bart van Strien <bart.bes@gmail.com>
2018-04-27 19:51:32 +0200

    Add a bunch of decoding code

--- lber/init.lua
+++ lber/init.lua
@@ -15,7 +15,7 @@ lber.handle = subrequire "handle"
 
 -- TODO: A way to handle errors
 
--- TODO: CER limitations
+-- TODO: All DER/CER limitation
 local universalTypes =
 {
 	[0x00] = { name = "EOF", forcePrimitive = true },
@@ -48,6 +48,13 @@ local universalTypes =
 	[0x1D] = { name = "CHARACTER STRING", der = { forcePrimitive = true } },
 	[0x1E] = { name = "BMPString" },
 }
+
+for i = 0x00, 0x1E do
+	local v = universalTypes[i]
+	if v then
+		universalTypes[v.name] = v
+	end
+end
 
 local function hasLimitation(type, mode, limitation)
 	if not type then return end
@@ -150,6 +157,257 @@ end
 
 function lber.parseLengthIndefinite(_)
 	return "indefinite" -- TODO: Try to find the length? Defer this to later?
+end
+
+universalTypes["BOOLEAN"].decode = function(tlv)
+	assert(tlv.length == 1, "Invalid BOOLEAN encoding")
+
+	local value = string.byte(tlv.value)
+	if tlv.mode == 'cer' or tlv.mode == 'der' then
+		assert(value == 255 or value == 0, "Invalid BOOLEAN encoding")
+	end
+	tlv.decoded = value ~= 0
+end
+
+universalTypes["INTEGER"].decode = function(tlv)
+	-- TODO: bignum?
+	local value = 0
+	for i = 1, #tlv.value do
+		value = bit.addOctetBE(value, string.byte(tlv.value, i))
+	end
+	-- TODO two's complement
+	tlv.decoded = value
+end
+universalTypes["ENUMERATED"].decode = universalTypes["INTEGER"].decode
+
+universalTypes["REAL"].decode = function(tlv)
+	if #tlv.value then -- +0
+		tlv.decoded = 0
+		return
+	end
+
+	local handle = lber.handle.fromString(tlv.value)
+	local first = lber.handle.readByte(handle)
+	if bit.band(first, 0x80) == 0x80 then
+		local sign = bit.band(first, 0x40) == 1 and -1 or 1
+		local base = bit.band(first, 0x30)
+		if base == 0x00 then
+			base = 2
+		elseif base == 0x10 then
+			base = 8
+		elseif base == 0x20 then
+			base = 16
+		else
+			error("Invalid base for REAL")
+		end
+		local factor = bit.rshift(bit.band(first, 0x0C), 2)
+		local exponentFormat = bit.band(first, 0x03)
+		local exponentOctets = exponentFormat + 1
+		if exponentFormat == 0x03 then
+			exponentOctets = lber.handle.readByte()
+			assert(exponentOctets > 0, "Invalid exponent format for REAL")
+		end
+
+		local exponent = 0
+		for _ = 1, exponentOctets do
+			exponent = bit.addOctetBE(exponent, lber.handle.readByte(handle))
+		end
+		-- TODO: Verify exponent
+		-- TODO: two's complement
+
+		local number = 0
+		while not lber.handle.isEof() do
+			number = bit.addOctetBE(number, lber.handle.readByte(handle))
+		end
+
+		local mantissa = sign * number * 2^factor
+		tlv.value = base ^ exponent * mantissa
+	elseif bit.band(first, 0xC0) == 0x00 then
+		-- TODO: decimal (ISO-6093)
+	elseif bit.band(first, 0xC0) == 0x40 then
+		if first == 0x40 then
+			tlv.value = math.huge
+		elseif first == 0x41 then
+			tlv.value = -math.huge
+		elseif first == 0x42 then
+			tlv.value = math.abs(0/0) -- nan
+		elseif first == 0x43 then
+			tlv.value = -0
+		end
+	end
+end
+
+universalTypes["BIT STRING"].decode = function(rootTLV)
+	local function decode(tlv)
+		assert(tlv.type.class == "universal" and
+			tlv.type.tagNumber == 0x03,
+			"Invalid BIT STRING encoding")
+
+		local handle = lber.handle.fromString(tlv.value)
+		if not tlv.type.isConstructed then
+			local unused = lber.handle.readByte(handle)
+			local bits = 0
+
+			local value = 0
+			while not lber.handle.isEof(handle) do
+				value = bit.addOctetBE(value, lber.handle.readByte(handle))
+				bits = bits + 8
+			end
+
+			return bit.rshift(value, unused), bits - unused
+		else
+			local value, bits = 0, 0
+
+			while not lber.handle.isEof(handle) do
+				local subTLV = lber.parseTLV(handle, tlv.mode)
+				if subTLV.type.class == "universal" and
+						subTLV.type.tagNumber == 0x00 then -- EOC
+					break
+				end
+
+				local subValue, subBits = decode(subTLV)
+
+				value = bit.bor(
+					bit.lshift(value, subBits),
+					subValue)
+				bits = bits + subBits
+			end
+		end
+	end
+
+	rootTLV.decoded = decode(rootTLV)
+end
+
+universalTypes["OCTET STRING"].decode = function(tlv)
+	if not tlv.type.isConstructed then
+		tlv.decoded = tlv.value
+	else
+		local handle = lber.handle.fromString(tlv.value)
+		local parts = {}
+
+		while not lber.handle.isEof(handle) do
+			local subTLV = lber.parseTLV(handle, tlv.mode)
+			assert(subTLV.type.class == "universal", "Invalid constructed OCTET STRING encoding")
+			if subTLV.type.tagNumber == 0x00 then -- EOC
+				break
+			end
+			assert(subTLV.type.tagNumber == 0x04, "Invalid constructed OCTET STRING encoding")
+
+			universalTypes["OCTET STRING"].decode(subTLV)
+			table.insert(parts, subTLV.decoded)
+		end
+
+		tlv.decoded = table.concat(parts)
+	end
+end
+
+universalTypes["NULL"].decode = function(tlv)
+	assert(tlv.length == 0, "Invalid NULL encoding")
+end
+
+universalTypes["SEQUENCE"].decode = function(tlv)
+	local handle = lber.handle.fromString(tlv.value)
+	local parts = {}
+
+	while not lber.handle.isEof(handle) do
+		local subTLV = lber.parseTLV(handle, tlv.mode)
+		lber.tryDecode(subTLV)
+		table.insert(parts, subTLV)
+	end
+
+	tlv.decoded = parts
+end
+-- TODO: DER required sorting
+universalTypes["SET"].decode = universalTypes["SEQUENCE"].decode
+
+universalTypes["OBJECT IDENTIFIER"].decode = function(tlv)
+	universalTypes["RELATIVE-OID"].decode(tlv)
+
+	local parts = tlv.decoded
+
+	-- Split the first part, as it is special
+	local first = math.floor(parts[1]/40)
+	first = math.min(first, 2)
+	parts[1] = parts[1] - first*40
+	table.insert(parts, 1, first)
+
+	parts.str = table.concat(parts, ".")
+end
+
+universalTypes["RELATIVE-OID"].decode = function(tlv)
+	local handle = lber.handle.fromString(tlv.value)
+	local parts = {}
+
+	local current = 0
+	while not lber.handle.isEof(handle) do
+		local octet = lber.handle.readByte(handle)
+		current = bit.bor(
+			bit.lshift(current, 7), -- NOTE: 7 bits, not 8
+			bit.band(octet, 0x7F))
+
+		if bit.band(octet, 0x80) == 0x00 then
+			table.insert(parts, current)
+			current = 0
+		end
+	end
+
+	assert(current == 0, "Unfinished OBJECT IDENTIFIER encoding")
+
+	parts.str = table.concat(parts, ".")
+	tlv.decoded = parts
+end
+
+local function decodeString(tlv)
+	if not tlv.type.isConstructed then
+		return tlv.value
+	else
+		local handle = lber.handle.fromString(tlv.value)
+		local parts = {}
+
+		while not lber.handle.isEof(handle) do
+			local subTLV = lber.parseTLV(handle, tlv.mode)
+			assert(subTLV.type.class == tlv.type.class, "Invalid constructed string encoding")
+			if subTLV.type.tagNumber == 0x00 then -- EOC
+				break -- TODO: reject in DER?
+			end
+			assert(subTLV.type.tagNumber == tlv.type.tagNumber, "Invalid constructed string encoding")
+
+			table.insert(parts, decodeString(subTLV))
+		end
+
+		return table.concat(parts)
+	end
+end
+
+universalTypes["UTF8String"].decode = function(tlv)
+	-- TODO: Validate and special encoding rules?
+	tlv.decoded = decodeString(tlv)
+end
+
+universalTypes["PrintableString"].decode = function(tlv)
+	-- TODO: Validate and special encoding rules?
+	tlv.decoded = decodeString(tlv)
+end
+
+local function decodeTime(tlv)
+	local str = decodeString(tlv)
+	-- TODO: ensure utf8
+	return str
+end
+
+universalTypes["UTCTime"].decode = function(tlv)
+	tlv.decoded = decodeTime(tlv)
+end
+
+function lber.tryDecode(tlv)
+	if tlv.type.class ~= "universal" then
+		return
+	end
+
+	local uType = universalTypes[tlv.type.tagNumber]
+	if uType and uType.decode then
+		uType.decode(tlv)
+	end
 end
 
 function lber.parseTLV(handle, mode)
